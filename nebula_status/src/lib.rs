61f85e15426d057275387b2f38afe06bc9ad34be
Michael Bryant <shadow53@shadow53.com>
2020-07-29 22:34:46 -0700

    Revert "nebula_status: Convert serde::Serialize to bytes, bump version"
    This reverts commit a04f7f692548978d125f8d6d66e294853810faa4.
    
    Failed to notice I was on the `tonic` branch, and didn't realize it
    wasn't checking compilation with the feature flag on. It runs into an
    issue with "conflicting implementations" of the trait.

--- nebula_status/src/lib.rs
+++ nebula_status/src/lib.rs
@@ -1,7 +1,5 @@
 use bytes::Bytes;
 use http::header::{self, HeaderMap, HeaderValue};
-#[cfg(feature = "serde-json")]
-use serde::Serialize;
 #[cfg(feature = "server-warp")]
 use http::response::Builder;
 pub use http::StatusCode;
@@ -14,7 +12,6 @@ use hyper::Body;
 /// Currently, the only automatic conversion that is supported is for Warp.
 ///
 use std::fmt::Debug;
-use std::convert::Into;
 #[cfg(feature = "server-warp")]
 use warp::{
     reject::{self, Reject, Rejection},
@@ -101,26 +98,6 @@ pub enum Error {
     NotStatus(Rejection),
 }
 
-/// Custom trait to convert a type into Bytes. Required to be defined here so it
-/// can be implemented on foreign types.
-pub trait IntoBytes {
-    fn into_bytes(self) -> Bytes;
-}
-
-impl<T: Into<Bytes>> IntoBytes for T {
-    fn into_bytes(self) -> Bytes {
-        self.into()
-    }
-}
-
-// Blanket implementation to convert Serializable types into JSON bytes
-#[cfg(feature = "serde-json")]
-impl<T> IntoBytes for T where T: Serialize {
-    fn into_bytes(self) -> Bytes {
-        serde_json::to_string(&self).into()
-    }
-}
-
 /// A trait alias for marking associated data inside of a container with the 
 /// traits necessary to be used.
 pub trait StatusInnerData: Clone + Debug + Send + Sync + 'static {}
@@ -128,8 +105,8 @@ impl<T: Clone + Debug + Send + Sync + 'static> StatusInnerData for T {}
 
 /// A trait alias for marking associated data with the traits necessary to be
 /// used.
-pub trait StatusData: IntoBytes + StatusInnerData {}
-impl<T: IntoBytes + StatusInnerData> StatusData for T {}
+pub trait StatusData: Into<Bytes> + StatusInnerData {}
+impl<T: Into<Bytes> + StatusInnerData> StatusData for T {}
 
 /// An empty type used by a Status without associated data.
 #[derive(Clone, Debug)]
@@ -186,7 +163,7 @@ impl Status {
         Status {
             c: code,
             data: data.clone(),
-            data_bytes: data.into_bytes(),
+            data_bytes: data.into(),
             h: HeaderMap::new(),
         }
     }
